45c2485b023594e5fae1574c0016ef4cf52d1110
mukhametzyanovra <rikitavi777@yandex.ru>
2023-07-10 18:10:57 +0300

    Add calcilator

--- src/main/java/ru/mukhametzyanov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/mukhametzyanov/springcourse/controllers/FirstController.java
@@ -1,7 +1,9 @@
 package ru.mukhametzyanov.springcourse.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +14,42 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println(name + " " + surname);
+//        System.out.println(name + " " + surname);
+        model.addAttribute("message", name + " " + surname);
         return "first/hello";
+
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam("a") int a,
+                                 @RequestParam("b") int b,
+                                 @RequestParam("action") String action, Model model) {
+        double result;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
